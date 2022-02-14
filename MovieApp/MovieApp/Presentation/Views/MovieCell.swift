0d38c4bd1afbb9bd5310bb2be78e69dad76d6688
jeff <xorgus1234@gmail.com>
2022-02-14 21:25:03 +0900

    Feat: MovieCell update 구현

--- MovieApp/MovieApp/Presentation/Views/MovieCell.swift
+++ MovieApp/MovieApp/Presentation/Views/MovieCell.swift
@@ -16,9 +16,40 @@ class MovieCell: UICollectionViewCell {
     @IBOutlet private weak var actorLabel: UILabel!
     @IBOutlet private weak var ratingLabel: UILabel!
     @IBOutlet private weak var favoriteButton: UIButton!
-
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        self.favoriteButton.imageView?.image = UIImage(named: "starUnFilled")
+    
+    private var task: ImageCancelable?
+    private var favoriteButtonAction: ((Bool) -> ())?
+    private var favoriteState: Bool
+    
+    required init?(coder: NSCoder) {
+        self.favoriteState = false
+        super.init(coder: coder)
+    }
+    
+    func update(_ movie: Movie ,buttonAction: @escaping (Bool) -> ()) {
+        self.titleLabel.text = movie.title
+        self.directorLabel.text = movie.director
+        self.actorLabel.text = movie.actors
+        self.ratingLabel.text = movie.rating
+        self.task = ImageDownloadManager.shared.load(urlString: movie.thumbnail) { data in
+            DispatchQueue.main.async { [weak self] in
+                self?.thumbnailImageView.image = UIImage(data: data)
+            }
+        }
+        self.favoriteState = FavoriteManager.shared.isFavorite(movie)
+        self.favoriteButtonAction = buttonAction
+        self.favoriteButton.setImage(self.favoriteState ? UIImage(named: "starFilled") : UIImage(named: "starUnfilled"),
+                                     for: .normal)
+    }
+    
+    func cancel() {
+        self.task?.downloadCancel()
+    }
+    
+    @IBAction func favoriteButtonTouched(_ sender: UIButton) {
+        self.favoriteState = !self.favoriteState
+        self.favoriteButtonAction?(self.favoriteState)
+        self.favoriteButton.setImage(self.favoriteState ? UIImage(named: "starFilled") : UIImage(named: "starUnfilled"),
+                                     for: .normal)
     }
 }
