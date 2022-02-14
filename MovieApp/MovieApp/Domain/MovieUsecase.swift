222cc7a2a9ed720c63e0d9405ee8bde7a3a0d0ec
jeff <xorgus1234@gmail.com>
2022-02-14 15:52:51 +0900

    Feat: MovieUsecase 구현

---
+++ MovieApp/MovieApp/Domain/MovieUsecase.swift
@@ -0,0 +1,31 @@
+//
+//  MovieUsecase.swift
+//  MovieApp
+//
+//  Created by TakHyun Jung on 2022/02/13.
+//
+
+import Foundation
+
+protocol MovieRepositoryInterface {
+    func query(query: String, completion: @escaping (Result<[Movie], Error>) -> Void)
+}
+
+final class MovieUsecase: MoviesUsecaseInterface {
+    private let repository: MovieRepositoryInterface
+    
+    init(repository: MovieRepositoryInterface) {
+        self.repository = repository
+    }
+    
+    func query(_ query: String, completion: @escaping (Result<[Movie], Error>) -> Void) {
+        self.repository.query(query: query) { result in
+            switch result {
+            case .success(let movies):
+                completion(.success(movies))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
